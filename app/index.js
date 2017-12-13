1796cd3ae87083f64ffe5c1683be00774249f90a
agrublev <agrublev@gmail.com>
2017-12-13 09:38:58 -0800

    added fs?

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@
 
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
+var fs = require('fs');
+
 var config = {};
 //var Cryptr = require('cryptr');
 
@@ -28,7 +30,7 @@ var a52project = yeoman.generators.Base.extend({
     this.prompt(prompts, function (props) {
       var Cryptr = require('cryptr'),
       cryptr = new Cryptr(props.password);
-      config = { 
+      config = {
         apiKey: cryptr.decrypt('865a60bb4a83cef4f2b73b2433238ffe846afc6b39176954afe5c4e6448671107411794a852304'),
           authDomain: cryptr.decrypt('a1617fbf7d99eec8f3ce0c1b7322a1e7a97abd550e0971009ddee7d7538b7540382b300c9f020a'),
           databaseURL: cryptr.decrypt('af676eaa6ac0a08ae59107116328a6e3ba36a753150e644798d6e1db5e84750b3f3232479e0c1478ca447ced6d29'),
@@ -36,7 +38,7 @@ var a52project = yeoman.generators.Base.extend({
           storageBucket: cryptr.decrypt('f22628ef2eccb892b6d65447'),
           messagingSenderId: cryptr.decrypt('552576775563')
         }
-        
+
       done();
     }.bind(this));
   },
